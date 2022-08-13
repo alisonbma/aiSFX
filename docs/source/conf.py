6ca8ceee1758af670e0c37efbc106c8e139d8333
Alison Bernice Ma <62447646+alisonbma@users.noreply.github.com>
2022-08-13 02:18:57 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,21 @@ release = '0.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import sys
 import os
+import six
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../../src'))
+
+# Mock modules......: Credit - https://github.com/marl/openl3/blob/main/docs/conf.py
+if six.PY3:
+    from unittest.mock import MagicMock
+else:
+    from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def getattr(cls, name):
+        return MagicMock()
+sys.modules.update((mod_name, Mock()) for mod_name in ["torch", "torch.nn", "torch.nn.functional"])
 
 extensions = [
     "sphinx.ext.duration",
